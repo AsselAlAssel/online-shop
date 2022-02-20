602e0f62bc79a640759d6f774ae7ad1ec084445c
Aseel <aseel1234500000@gmail.com>
2022-02-20 19:31:30 +0200

    add Footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import classes from "./App.module.scss";
+import Footer from "./components/Footer/Footer";
 import Head from "./components/Head/Head";
 import Home from "./components/Home/Home";
 import Loading from "./components/Loading/Loading";
@@ -11,26 +12,23 @@ const App = () => {
   useEffect(() => {
     setTimeout(() => {
       fetch("https://fakestoreapi.com/products")
-      .then((res) => res.json())
-      .then((json) => setProducts(json));
+        .then((res) => res.json())
+        .then((json) => setProducts(json));
     }, 1000);
-  
   }, []);
- 
 
   return (
     <React.Fragment>
-
       {products.length === 0 ? (
         <Loading />
       ) : (
         <BrowserRouter>
           <Head />
           <Routes>
-            <Route exact  path="/" element={<Home products={products}/>}/>
+            <Route exact path="/" element={<Home products={products} />} />
           </Routes>
+          <Footer />
         </BrowserRouter>
-      
       )}
     </React.Fragment>
   );
