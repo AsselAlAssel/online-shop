4bd7c17e95db56fff6ba64f938c53d789d706e84
Aseel <aseel1234500000@gmail.com>
2022-02-20 18:06:13 +0200

    add useeffect and the api and the router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";import classes from "./App.module.scss"
+import React, { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import classes from "./App.module.scss";
 import Head from "./components/Head/Head";
+import Home from "./components/Home/Home";
+import Loading from "./components/Loading/Loading";
 // import Home from "./components/home";
 const App = () => {
+  const [products, setProducts] = useState([]);
+  // fetch the data from fake api
+  useEffect(() => {
+    setTimeout(() => {
+      fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((json) => setProducts(json));
+    }, 1000);
+  
+  }, []);
+ 
+
   return (
-    <BrowserRouter>
-      <Head/>
-      <Routes>
-      </Routes>
-    </BrowserRouter>
+    <React.Fragment>
+
+      {products.length === 0 ? (
+        <Loading />
+      ) : (
+        <BrowserRouter>
+          <Head />
+          <Routes>
+            <Route exact  path="/" element={<Home products={products}/>}/>
+          </Routes>
+        </BrowserRouter>
+      
+      )}
+    </React.Fragment>
   );
 };
 export default App;
-
