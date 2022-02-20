c942a444734c68faef5a9c36f0f96aeb50cf0fa7
Aseel <aseel1234500000@gmail.com>
2022-02-20 14:07:50 +0200

    ui button and its style

---
+++ src/components/UI/Button.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import classes from "./Button.module.scss"
+ const Button =(props)=>{
+     return <button className={classes.button}onClick={props.onclick}>{props.children}</button>
+
+ }
+ export default Button
