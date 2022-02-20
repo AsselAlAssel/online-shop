9dba219ecdbd0ab09a45fb9fea7c2d860e23381c
Aseel <aseel1234500000@gmail.com>
2022-02-20 18:07:52 +0200

    loading page

---
+++ src/components/Loading/Loading.js
@@ -0,0 +1,18 @@
+import classes from "./Loading.module.scss"
+const Loading =()=>{
+    return(
+        <div className={classes.loading}>
+        <div className={classes.obj}></div>
+        <div className={classes.obj}></div>
+        <div className={classes.obj}></div>
+        <div className={classes.obj}></div>
+        <div className={classes.obj}></div>
+        <div className={classes.obj}></div>
+        <div className={classes.obj}></div>
+        <div className={classes.obj}></div>
+        <div className={classes.obj}></div>
+        <div className={classes.obj}></div>
+    </div>
+    )
+}
+export default  Loading;
