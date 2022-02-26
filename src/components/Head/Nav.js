944b43dc65912ea5b00df210968f6fdd2bb54f2c
Aseel <aseel1234500000@gmail.com>
2022-02-26 17:02:53 +0200

    add funcion for show  menu and cart

--- src/components/Head/Nav.js
+++ src/components/Head/Nav.js
@@ -6,19 +6,28 @@ import cart from "../assets/imgs/cart.png";
 import { NavLink } from "react-router-dom";
 
 const Nav = () => {
+  // show the menu
+  const showMenu = () => {
+  
+    document.getElementById("menu-container").style.marginLeft = "0px";
+  };
+  // show the cart
+  const showCart = () => {
+    document.getElementById("cart-container").style.marginRight = "0px";
+  };
   return (
     <nav className={classes.nav}>
       <div className={classes.menu}>
-        <img src={menu} alt="menu" />
+        <img src={menu} alt="menu icon" onClick={showMenu}/>
       </div>
 
       <div className={classes.logo}>
         <NavLink to="/">
-          <img src={logo} alt="logo" />
+          <img src={logo} alt="logo fot the shop" />
         </NavLink>
       </div>
       <div className={classes.cart}>
-        <img src={cart} alt="cart" />
+        <img src={cart} alt="cart icon" onClick={showCart} />
       </div>
     </nav>
   );
