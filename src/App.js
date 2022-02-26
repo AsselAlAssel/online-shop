6e364fe06aeea803d7063fb556d2ce5632cc5b89
Aseel <aseel1234500000@gmail.com>
2022-02-26 17:01:49 +0200

    add cart ,menu,shop components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import classes from "./App.module.scss";
+import CartContainer from "./components/Cart/CartContainer";
 import Footer from "./components/Footer/Footer";
 import Head from "./components/Head/Head";
 import Home from "./components/Home/Home";
 import Loading from "./components/Loading/Loading";
+import MenuContainer from "./components/Menu/MenuContainer";
+import Shop from "./components/Shop/Shop";
 // import Home from "./components/home";
 const App = () => {
   const [products, setProducts] = useState([]);
@@ -24,8 +27,11 @@ const App = () => {
       ) : (
         <BrowserRouter>
           <Head />
+          <MenuContainer/>
+          <CartContainer/>
           <Routes>
             <Route exact path="/" element={<Home products={products} />} />
+            <Route exact path="/shop" element={<Shop products={products} />} />
           </Routes>
           <Footer />
         </BrowserRouter>
