1cb913d136c11cae3be86d02566f1fdb33f3f3a9
Aseel <aseel1234500000@gmail.com>
2022-02-27 18:23:12 +0200

    add functions

--- src/components/Store/Cartprovider.js
+++ src/components/Store/Cartprovider.js
@@ -1,13 +1,55 @@
 import React, { useReducer } from "react";
 import CartContext from "./CatrContex";
+import { updateCartLis, updateCartLisWhenRemove } from "./Functions";
 
-const cartReducer = (state, action) => {};
+const defaultState = {
+  cartList: [],
+  totalAmount: 0,
+};
+
+const cartReducer = (state, action) => {
+  if (action.type === "Add") {
+    return {
+      cartList: updateCartLis(state.cartList, action.value),
+      totalAmount: state.totalAmount + action.value.price,
+    };
+  } else if (action.type === "BluseOrMinus") {
+    if (action.value.sign === -1) {
+      return {
+        cartList: updateCartLisWhenRemove(state.cartList, action.value.index),
+        totalAmount: state.totalAmounts - 1,
+      };
+    } else {
+      state.cartList[action.value.index].amount += 1;
+      return {
+        cartList: state.cartList,
+        totalAmounts: state.totalAmounts + 1,
+      };
+    }
+  }
+  return defaultState;
+};
 
 const CartProvider = (props) => {
-  const [CartState, cartDispatch] = useReducer(cartReducer, {
-    cartList: [],
-    totalAmount: 0,
-  });
-  return <CartContext.Provider>{props.children}</CartContext.Provider>;
+  const [CartState, cartDispatch] = useReducer(cartReducer, defaultState);
+
+  // add item from btn add to cart
+  const AddToCart = (data) => {
+    cartDispatch({ type: "Add", value: data });
+  };
+  const BluseOrMinus = (data) => {
+    cartDispatch({ type: "BluseOrMinus", value: data });
+  };
+  const context = {
+    cartList: CartState.cartList,
+    totalAmount: CartState.totalAmount,
+    addItem: AddToCart,
+    blusOrMinus: BluseOrMinus,
+  };
+  return (
+    <CartContext.Provider value={context}>
+      {props.children}
+    </CartContext.Provider>
+  );
 };
 export default CartProvider;
---
+++ src/components/Store/Functions.js
@@ -0,0 +1,24 @@
+export const updateCartLis = (CartList, item) => {
+    console.log(CartList)
+    for (let index = 0; index < CartList.length; index++) {
+      const ele = CartList[index];
+      if (ele.id === item.id) {
+        
+        ele.amount += 1;
+        console.log(ele.amount)
+        return CartList;
+      }
+    }
+
+    console.log("ff")
+    item['amount']=1;
+    CartList = [...CartList, item];
+    return CartList;
+  };
+  export const updateCartLisWhenRemove = (CartList, index) => {
+    CartList[index].amount -= 1;
+    if (CartList[index].amount === 0) {
+      CartList.splice(index, 1);
+    }
+    return CartList;
+  };
