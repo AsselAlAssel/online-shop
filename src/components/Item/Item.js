cbaa1c362e41c27b39656d7b36c48dd3925e13ae
Aseel <aseel1234500000@gmail.com>
2022-02-27 18:22:11 +0200

    cahnge the navLink

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -3,8 +3,14 @@ import classes from "./Item.module.scss";
 const Item = (props) => {
   return (
     <div className={classes["item"]}>
-      <NavLink to="/d">
-        <img src={props.data.image} alt="" />    
+      <NavLink
+        to={{
+          pathname: "/element",
+          hash: `${props.data.id}`
+         
+        }}
+      >
+        <img src={props.data.image} alt="" />
       </NavLink>
       <p>{props.data.title}</p>
       <p>$ {props.data.price}</p>
